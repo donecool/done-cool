e90b827544f7f2c9141f2945a5eb50d7d0c5aa01
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2017-04-06 20:15:57 -0400

    Array of bindings

--- src/ui/components/done-cool/component.ts
+++ src/ui/components/done-cool/component.ts
@@ -1,5 +1,22 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class DoneCool extends Component {
+  @tracked state = {
+    items: [""]
+  }
 
+  input(index, { target: { value }}) {
+    let items = this.state.items.slice();
+
+    items[index] = value
+
+    if (items.every(i => i.length > 0)) {
+      items.push("");
+    }
+
+    this.state = {
+      ...this.state,
+      items
+    }
+  }
 }
