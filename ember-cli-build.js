a5b0d4fab9cb4537cc8bca73652dd4cc7ab5240a
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2017-04-06 21:12:55 -0400

    Seed!

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,42 @@
 'use strict';
 
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
+const harvester = require('seed-harvester');
+const packer = require('seed-packer');
+
+const Funnel = require('broccoli-funnel');
+const concat = require('broccoli-concat');
+const path = require('path');
+const fs = require('fs');
+const compileSass = require('broccoli-sass');
+
+// Hack to include seed-harvester, until we have a way of specifying our own
+// includePaths for sass
+class CoolApp extends GlimmerApp {
+  cssTree() {
+    let stylesPath = path.join(this.srcPath, 'ui', 'styles');
+    if (fs.existsSync(stylesPath)) {
+      // Compile SASS if app.scss is present
+      // (this works with imports from app.scss)
+      let scssPath = path.join(stylesPath, 'app.scss');
+      if (fs.existsSync(scssPath)) {
+        return compileSass([stylesPath].concat(harvester()), 'app.scss', 'app.css', {
+          annotation: 'Funnel: scss'
+        });
+      }
+      // Otherwise concat all the css in the styles dir
+      return concat(new Funnel.default(stylesPath, {
+        include: ['**/*.css'],
+        annotation: 'Funnel: css'
+      }), { outputFile: 'app.css' });
+    }
+  }
+}
 
 module.exports = function(defaults) {
-  let app = new GlimmerApp(defaults, {
-    // Add options here
+  packer();
+
+  let app = new CoolApp(defaults, {
   });
 
   // Use `app.import` to add additional libraries to the generated
