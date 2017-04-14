dc0c3df2db41b3c9cb2ac251fc38f226d6ad4d01
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-04-14 18:18:56 -0400

    100% perfect bug free auto emoji

--- src/utils/auto-emoji.ts
+++ src/utils/auto-emoji.ts
@@ -1,5 +1,22 @@
 import emoji from 'emojilib';
 
+const Cool = emoji.lib.sunglasses.char
+
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 export default function AutoEmoji(text: string) : string {
-  return emoji.lib.grimacing.char;
+  let words = text.trim().split(/\s+/);
+  let matches = emoji.ordered.filter(function(e) {
+    let { keywords } = emoji.lib[e];
+
+    return words.some(w => keywords.some(k => k.includes(w) || w.includes(k)));
+  });
+
+  if (text.trim().length === 0 || matches.length === 0) {
+    return Cool;
+  }
+
+  return emoji.lib[random(matches)].char;
 }
