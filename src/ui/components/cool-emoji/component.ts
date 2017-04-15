43a79debdb443ec14b4b169665b0f197799af880
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-04-15 01:33:37 -0400

    Remove unused component

--- src/ui/components/cool-emoji/component.ts
+++
@@ -1,8 +0,0 @@
-import Component, { tracked } from '@glimmer/component';
-import AutoEmoji from '../../../utils/auto-emoji';
-
-export default class CoolEmoji extends Component {
-  get emoji() {
-    return AutoEmoji(this.args.text || '');
-  }
-};
